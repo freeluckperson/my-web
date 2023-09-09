1789ddf7f649b7fd3980c0e7c6fb183c80a835ae
freeluckperson <gestionecesaria@gmail.com>
2023-09-09 17:48:20 -0400

    9/9/2023 commit 1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./About.css";
+import cards from "./cardJson.json";
 
 const About = () => {
-  return (
-    <div>
-      <h3>SOY About</h3>
-    </div>
-  )
-}
+  console.log(cards);
 
-export default About
+  return (
+    <div className="card-container">
+      {cards.results.map((card) => {
+        return (
+          <Card style={{ width: "18rem" }} >
+            <Card.Img variant="top" src={card.image} />
+            <Card.Body>
+              <Card.Title>{card.title}</Card.Title>
+              <Card.Text>{card.description}</Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
+
+export default About;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,6 +15,7 @@ function Header() {
           <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
+           
           </Nav>
         </Navbar.Collapse>
       </Container>
